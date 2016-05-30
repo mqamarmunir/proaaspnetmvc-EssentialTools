dfd4500513b38a28dc8e7c8dc77081e26d2f6455
mqamarmunir <qamar.nust@gmail.com>
2016-05-30 16:34:51 +0500

    Initial Commit

---
+++ EssentilTools/EssentilTools/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentilTools.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
+    }
+
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+    public class LinqValueCalculator:IValueCalculator
+    {
+        private IDiscountHelper discounter;
+        public LinqValueCalculator(IDiscountHelper discountParam)
+        {
+            discounter = discountParam;
+        }
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private IValueCalculator calc;
+        public ShoppingCart(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
